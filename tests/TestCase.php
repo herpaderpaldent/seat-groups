b655bebed6583a4dc17427ee08334f7d8db200f3
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-10-09 17:22:05 +0200

    More testing: * added mariadb as host.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,11 +13,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
-    protected function getPackageProviders($app)
-    {
-        return [GroupsServiceProvider::class];
-    }
-
     /**
      * Setup the test environment.
      */
@@ -25,10 +20,30 @@ abstract class TestCase extends Orchestra
     {
         parent::setUp();
 
+        /*$this->loadMigrationsFrom([
+            '--database' => 'testbench',
+            '--realpath' => realpath(__DIR__ . '/database/migrations')
+            ]);*/
         $this->artisan('migrate', ['--database' => 'testbench']);
         $this->withFactories(__DIR__ . '/database/factories');
 
         // and other test setup steps you need to perform
+    }
+
+
+    /**
+     * Get application providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Orchestra\Database\ConsoleServiceProvider::class,
+            GroupsServiceProvider::class
+        ];
     }
 
     /**
@@ -39,11 +54,16 @@ abstract class TestCase extends Orchestra
      */
     protected function getEnvironmentSetUp($app)
     {
+        //TODO: find a better way or use dedicated DB
         // Setup default database to use sqlite :memory:
         $app['config']->set('database.default', 'testbench');
         $app['config']->set('database.connections.testbench', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
+            'driver'   => 'mysql',
+            'host' => env('DB_HOST', 'mariadb'),
+            'port'        => env('DB_PORT', '3306'),
+            'database'    => env('DB_DATABASE', 'seat-dev'),
+            'username'    => env('DB_USERNAME', 'seat'),
+            'password'    => env('DB_PASSWORD', 'seatseat'),
             'prefix'   => '',
         ]);
     }
