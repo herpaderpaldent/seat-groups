0233f209cb484e73b7704c27e6aff4e6829f6281
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-10-09 11:47:10 +0200

    Basic Test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,4 +18,34 @@ abstract class TestCase extends Orchestra
         return [GroupsServiceProvider::class];
     }
 
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->artisan('migrate', ['--database' => 'testbench']);
+        $this->withFactories(__DIR__ . '/database/factories');
+
+        // and other test setup steps you need to perform
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
 }
