112a048cec3ba8d3494387aba2e05aba3da1e8f6
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-11-28 13:44:26 +0100

    prevent managed-groups from throwing null object exceptions because of missing main character

--- src/resources/views/partials/modal-partials/modal-name.blade.php
+++ src/resources/views/partials/modal-partials/modal-name.blade.php
@@ -1,5 +1,5 @@
 {!! img('auto', $row->main_character_id, 64, ['class' => 'img-circle eve-icon small-icon'],false) !!}
-{{ $row->main_character->name }} ({{$row->users->map(function($user) { return $user->name; })->implode(', ')}})
+{{ optional($row->main_character)->name }} ({{$row->users->map(function($user) { return $user->name; })->implode(', ')}})
 
 @if($row->pivot->is_manager === 1)
   (Manager)
