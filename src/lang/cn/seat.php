a02dea21bd407dcf4883697c1019471447567ea5
神楽坂·喵 <yuronglong1996@gmail.com>
2019-11-08 10:50:20 +0800

    Add Chinese Translation

---
+++ src/lang/cn/seat.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Chinese Translate
+ * User: KagurazakaNyaa
+ * Date: 11.08.2019
+ * Time: 10:38.
+ */
+
+return [
+
+    'seat_groups'           => 'SeAT 组',
+    'seat_groups_dashboard' => '信息中心',
+
+    'seat_groups_autogroup' => '自动 SeAT 组',
+    'seat_groups_autogroup_description' => '你将会根据你的军团关系被自动分配到这些 SeAT 组中',
+
+    'seat_groups_opengroup' => '开放 SeAT 组',
+    'seat_groups_opengroup_description' => '你可以根据你自己的意愿随意加入或退出这些 SeAT 组',
+
+    'seat_groups_managedgroup'             => '受管理的 SeAT 组',
+    'seat_groups_managedgroup_description' => '你可以申请加入这类组，它们的管理员将会审核你的请求。',
+    'seat_groups_manager'                  => '管理员',
+    'seat_join_opengroup'                  => '加入',
+    'seat_leave_opengroup'                 => '退出',
+    'seat_leave_waitlist'                  => '撤回申请',
+    'seat_join_waitlist'                   => '申请',
+    'seat_leave_managedgroup'              => '离开',
+    'seat_deny_managedgroup'               => '拒绝',
+    'seat_accept_managedgroup'             => '接受',
+    'seat_removefrom_managedgroup'         => '移除',
+    'manage_members'                       => '管理成员',
+    'available_seatgroups'                 => '可用作管理员的 SeAT 组',
+
+    'seat_groups_hiddengroup' => '隐藏的 SeAT 组',
+    'seat_groups_user'        => '用户',
+    'seat_groups_adduser'     => '添加用户',
+
+    'seat_groups_create'      => '创建',
+    'seat_groups_create_new'  => '创建一个新的 SeAT 组',
+    'seat_groups_name'        => '名称',
+    'seat_groups_description' => 'SeAT 组描述',
+    'seat_groups_type'        => '选择 SeAT 组类型',
+    'seat_groups_role'        => '选择相应的 SeAT 角色',
+    'seat_groups_add'         => '添加新的 SeAT 组',
+
+    'seat_groups_edit'    => '添加新的 SeAT 组',
+    'seat_groups_editing' => '编辑',
+    'seat_groups_update'  => '更新 SeAT 组',
+
+    'seat_groups_about' => '关于',
+
+    'available_alliances' => '可用的联盟',
+    'add_alliance'        => '添加联盟',
+
+    //Event Log
+    'event' => '事件',
+
+];
